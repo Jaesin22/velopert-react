4264c2e461ec3c81b5a054e55324479cf1843289
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-06-02 17:52:27 +0900

    Dialog

---
+++ src/components/Dialog.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+
+const dackBackground = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
