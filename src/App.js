76d417991ff2e05ebf90d2eab9e475c97283f084
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-06-14 16:59:11 +0900

    api 연동 기본

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
-import TodoTemplate from "./components/TodoTemplate";
-import TodoHead from "./components/TodoHead";
-import TodoList from "./components/TodoList";
-import TodoCreate from "./components/TodoCreate";
-import { TodoProvider } from "./TodoProvider";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background: #e9ecef;
-  }
-`;
+import Users from "./Users";
 
 function App() {
-  return (
-    <TodoProvider>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
-    </TodoProvider>
-  );
+  return <Users />;
 }
 
 export default App;
